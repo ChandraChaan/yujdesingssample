85c71cf4edeb56bf76dc14c3db5db6bc6c7cccec
Chandra <chandra.dumpla@iesoftek.com>
2022-01-16 20:23:43 +0530

    added splash screen

--- lib/Config/app_pages.dart
+++ lib/Config/app_pages.dart
@@ -1,12 +1,20 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:yujdesingssample/Application/Binding/homepage_binding.dart';
-
+import 'package:yujdesingssample/Presentation/login/loginScreenView.dart';
+import 'package:yujdesingssample/Presentation/splashScreen/splashScreen.dart';
 
 part 'app_route.dart';
 
 class AppPages {
-  static const INITIAL = Routes.login;
+  static const INITIAL = Routes.splash;
   static final routes = [
-    // GetPage(name: Routes.login, page: () => loginPage(), binding: MainBinding()),
+    GetPage(
+        name: Routes.splash,
+        page: () => splashScreen(),
+        binding: MainBinding()),
+    GetPage(
+        name: Routes.login,
+        page: () => loginScreen(),
+        binding: MainBinding()),
   ];
 }
